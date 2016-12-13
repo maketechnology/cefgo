99c14aed3bd9e5ba13ec651d6e00828669474df7
Guillermo Zunino <guillez@gmail.com>
2016-12-13 18:53:30 -0300

    init

--- hello.go
+++ hello.go
@@ -4,6 +4,8 @@ package main
 // #cgo LDFLAGS: -lcef -L/home/gzunino/Downloads/cef_binary_3.2883.1539.gd7f087e_linux64/Release/
 // #include <stdlib.h>
 // #include "include/capi/cef_app_capi.h"
+//
+// typedef struct _cef_browser_process_handler_t* (*get_browser_process_handler)(struct _cef_app_t* self);
 import "C"
 import (
 	"fmt"
@@ -11,6 +13,11 @@ import (
 )
 
 var _Argv []*C.char = make([]*C.char, len(os.Args))
+
+// export
+func GetBrowserProcessHandler(self *C.struct__cef_app_t) *C.struct__cef_browser_process_handler_t {
+	return nil
+}
 
 func main() {
 	fmt.Println("IN GO")
@@ -26,6 +33,36 @@ func main() {
 		fmt.Println("EXIT GO", exitCode)
 		os.Exit(int(exitCode))
 	}
+
+	// Initialize cef_settings_t structure.
+	var cefSettings *C.struct__cef_settings_t
+	cefSettings = (*C.struct__cef_settings_t)(
+		C.calloc(1, C.sizeof_struct__cef_settings_t))
+	cefSettings.size = C.sizeof_struct__cef_settings_t
+
+	createSettings(cefSettings)
+
+	var _AppHandler *C.cef_app_t // requires reference counting
+	_AppHandler = (*C.cef_app_t)(
+		C.calloc(1, C.sizeof_cef_app_t))
+
+	f := C.get_browser_process_handler(GetBrowserProcessHandler)
+	//f := getBrowserProcessHandler
+	//fp := unsafe.Pointer(&f)
+	//_AppHandler.get_browser_process_handler = fp
+
+	fmt.Println("cef_initialize")
+	C.cef_initialize(_MainArgs, cefSettings, _AppHandler, nil)
+
+	fmt.Println("Calling runMessageLoop")
+	// Run the Cef message loop. This will block until CefQuitMessageLoop() is
+	// called.
+	C.cef_run_message_loop()
+
+	fmt.Println("Calling shutdown")
+	// Shut down Cef.
+	C.cef_shutdown()
+
 	fmt.Println("DONE GO")
 }
 
@@ -38,4 +75,25 @@ func fillMainArgs(mainArgs *C.struct__cef_main_args_t) {
 	}
 	mainArgs.argc = C.int(len(os.Args))
 	mainArgs.argv = &_Argv[0]
+}
+
+func createSettings(cefSettings *C.struct__cef_settings_t) {
+	//C.cef_string_from_utf8(cachePath, C.strlen(cachePath), &cefSettings.cache_path)
+
+	//cefSettings.log_severity = (C.cef_log_severity_t)(C.int(settings.LogSeverity))
+
+	//var logFile *C.char = C.CString("")
+	//defer C.free(unsafe.Pointer(logFile))
+	//C.cef_string_from_utf8(logFile, C.strlen(logFile), &cefSettings.log_file)
+
+	//var resourcesDirPath *C.char = C.CString("")
+	//defer C.free(unsafe.Pointer(resourcesDirPath))
+	//C.cef_string_from_utf8(resourcesDirPath, C.strlen(resourcesDirPath), &cefSettings.resources_dir_path)
+
+	//var localesDirPath *C.char = C.CString(settings.LocalesDirPath)
+	//defer C.free(unsafe.Pointer(localesDirPath))
+	//C.cef_string_from_utf8(localesDirPath, C.strlen(localesDirPath),
+	//	&cefSettings.locales_dir_path)
+
+	cefSettings.no_sandbox = C.int(1)
 }
